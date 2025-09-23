878fc9104b60d62b61641cf693176488efbb5d97
JAIDEEP <Jaideepindia99@gmail.com>
2025-09-24 01:51:51 +0530

    ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.mario.it"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mario.it"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
